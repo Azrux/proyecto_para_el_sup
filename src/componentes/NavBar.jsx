152cb5dbfa00965282cf76b405a05e18467e6811
William Salas <willbullet52@gmail.com>
2022-12-03 12:26:14 -0500

    componente de countries hecho con clases, descubrí el error :v

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,38 +1,20 @@
 import { Link } from "react-router-dom";
-import s from '../styles/NavBar.module.css'
+import s from "../styles/NavBar.module.css";
 
 export default function NavBar() {
-<<<<<<< HEAD
 	return (
-		<div>
+		<div className={s.NavBar}>
+			<p>(IMG)</p>
+			<p>Empresa</p>
 			<Link to="/">
 				<p>Home</p>
 			</Link>
 			<Link to="/about">
 				<p>About</p>
 			</Link>
-			<Link to="/copas">
-				<p>Copas</p>
+			<Link to="/countries">
+				<p>Países</p>
 			</Link>
 		</div>
 	);
 }
-=======
-
-  return(
-    <div className={s.NavBar}>
-      <p>(IMG)</p>
-      <p>Empresa</p>
-      <Link to='/'>
-        <p>Home</p>
-      </Link>
-      <Link to='/about'>
-        <p>About</p>
-      </Link>
-      <Link to='/countries'>
-        <p>Países</p>
-      </Link>
-    </div>
-  )
-}
->>>>>>> 7ac1c74be7b8620c1225ef28adc1604d12a51401
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,21 +1,21 @@
-import { GET_ALL_COUNTRIES } from "../actions"
+import { GET_ALL_COUNTRIES } from "../actions";
 
 export const initialState = {
-  countries: []
-}
+	countries: [],
+};
 
 export default function reducer(state = initialState, action) {
-  switch(action.type){
-    case GET_ALL_COUNTRIES: {
-      return {
-        ...state,
-        countries: action.payload
-      }
-    }
-    default: {
-      return {
-        ...state
-      }
-    }
-  }
+	switch (action.type) {
+		case GET_ALL_COUNTRIES: {
+			return {
+				...state,
+				countries: action.payload,
+			};
+		}
+		default: {
+			return {
+				...state,
+			};
+		}
+	}
 }
